17fff242fb5c89a598f4c0c09499d13af2afed63
EmilAlexLarsson <emilalexlarsson@gmail.com>
2026-06-03 23:33:26 +0200

    ändrat år i footer

--- carnordic-client/src/components/layout/Footer.jsx
+++ carnordic-client/src/components/layout/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
     <footer className="footer">
       <p>info@carnordic.se</p>
       <p>040-123 45 67</p>
-      <p>© 2025 Carnordic Technology</p>
+      <p>© 2026 Carnordic Technology</p>
     </footer>
   );
 }
